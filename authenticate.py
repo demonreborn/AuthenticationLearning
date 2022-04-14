e9f7556b8e1fd2294c70074f1255a62c46e7bb99
Jesse <JesseBefus@hotmail.com>
2022-04-13 23:11:02 -0600

    Update authenticate.py
    I like to tell people when stuff is useless

--- authenticate.py
+++ authenticate.py
@@ -4,6 +4,7 @@ import cgi
 
 form = cgi.FieldStorage
 
+# This is basically useless at the moment....
 if form.getvalue('username') == "Jesse" & form.getvalue('password') == "123456":
     print("<h1>Authorized!</h1>")
 else:
